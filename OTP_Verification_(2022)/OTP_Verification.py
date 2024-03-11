fd056816a8da28d083233ff83fd4257d6e8ded0b
YadavDipak <yadavdipak2911@gmail.com>
2024-03-11 17:17:37 +0530

    first commit

---
+++ OTP_Verification_(2022)/OTP_Verification.py
@@ -0,0 +1,13 @@
+import math
+import random
+
+def generateOTP():
+    digits = "0123456789"
+    OTP = ""
+
+    for i in range(5):
+        OTP += digits[math.floor(random.random()*10)]
+    return OTP
+
+if __name__ == "__main__":
+    print("OTP of 5 digits :",generateOTP())
